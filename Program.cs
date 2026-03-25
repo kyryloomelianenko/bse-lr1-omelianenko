72fa5d6ae398860b635cf1f187b1a8c9f7678130
Kyrylo <kyrylo.omelianenko@nure.ua>
2026-03-25 21:28:31 +0200

    Add project files

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello World");
+
+        // Запит першого числа
+        Console.Write("Введіть перше число: ");
+        int a = int.Parse(Console.ReadLine());
+
+        // Запит другого числа
+        Console.Write("Введіть друге число: ");
+        int b = int.Parse(Console.ReadLine());
+
+        // Обчислення суми
+        int sum = a + b;
+
+        // Виведення результату
+        Console.WriteLine($"Результат: {a} + {b} = {sum}");
+    }
+}
